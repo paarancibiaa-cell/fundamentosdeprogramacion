eae9befc164f619c2eb5f72557da04cbdbe044a3
unknown <SSDD@POAPCA01LC0420.duoc.acad>
2026-06-22 22:06:01 -0400

    funciones clase 1

---
+++ funciones.py
@@ -0,0 +1,20 @@
+#def funcion_sencilla():
+ #print("Hola, soy una función sencilla.")
+# En este segmento agregar
+# todo lo que requieres que esta función realice
+# Sumar, restar, Convertir archivos, etc..
+# Llamando a la función, ejecuta todo el contenido de la función
+#funcion_sencilla()
+#def suma():
+ #num1 = 3
+ #num2 = 5
+ #return(num1 + num2)
+# Llamando a la función, ejecuta todo el contenido de la función
+#print("la suma es: ", suma())
+def suma(a,b):
+ sumar = a + b
+ print(f"La suma de los argumentos es: {sumar}")
+
+num1 = int(input("Ingrese primer número: "))
+num2 = int(input("Ingrese segundo número: "))
+suma(num1,num2)
